2c58c68863b0b6bb7d9dd46390a4baa1116ffd92
04041b <longtrip.com@gmail.com>
2025-03-06 23:16:19 -0800

    updated test

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,35 +1,35 @@
-const os = require('os');
-const path = require('path');
-const { spawn, spawnSync } = require('child_process');
+const os = require("os");
+const path = require("path");
+const { spawn, spawnSync } = require("child_process");
 
 // keep track of the `tauri-driver` child process
 let tauriDriver;
 
 exports.config = {
-  specs: ['./tests/*.js'],
+  specs: ["./test/specs/*.js"],
   maxInstances: 1,
   capabilities: [
     {
       maxInstances: 1,
-      'tauri:options': {
-        application: '../../target/release/mobile-multithread-downloader',
+      "tauri:options": {
+        application: "./src-tauri/target/release/app",
       },
     },
   ],
-  reporters: ['spec'],
-  framework: 'mocha',
+  reporters: ["spec"],
+  framework: "mocha",
   mochaOpts: {
-    ui: 'bdd',
+    ui: "bdd",
     timeout: 60000,
   },
 
   // ensure the rust project is built since we expect this binary to exist for the webdriver sessions
-  onPrepare: () => spawnSync('cargo', ['build' , '--no-bundle']),
+  onPrepare: () => spawnSync("cargo", ["build", "--release"]),
 
   // ensure we are running `tauri-driver` before the session starts so that we can proxy the webdriver requests
   beforeSession: () =>
     (tauriDriver = spawn(
-      path.resolve(os.homedir(), '.cargo', 'bin', 'tauri-driver'),
+      path.resolve(os.homedir(), ".cargo", "bin", "tauri-driver"),
       [],
       { stdio: [null, process.stdout, process.stderr] }
     )),
